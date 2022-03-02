5cbbab1fb043ecc37094b82bc3ec945b409f65f4
AfganJafarov <74818011+AfganJafarov@users.noreply.github.com>
2022-03-02 23:05:20 +0400

    first

---
+++ src/main/java/com/orient/springpro2/Springpro2Application.java
@@ -0,0 +1,13 @@
+package com.orient.springpro2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springpro2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springpro2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/orient/springpro2/Springpro2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orient.springpro2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springpro2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
